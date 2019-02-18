f2c5b9af466f934645e4899df69465d19faf684b
Krzysztof Kielak <krzysztof.kielak@gmail.com>
2019-02-18 22:35:47 +0100

    test.z80 renamed to scroll.z80 and build script modified

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pasmo --tapbas test.z80 test.tap \
-    && /usr/local/zesarux/bin/zesarux test.tap
+pasmo --tapbas scroll.z80 scroll.tap \
+    && /usr/local/zesarux/bin/zesarux scroll.tap
