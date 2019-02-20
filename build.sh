ba8c8115b6f251f3c94039ae4930e41c3c111a02
Krzysztof Kielak <krzysztof.kielak@gmail.com>
2019-02-20 20:16:14 +0100

    file shuffling

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pasmo --tapbas scroll.z80 scroll.tap \
-    && /usr/local/zesarux/bin/zesarux scroll.tap
+pasmo --tapbas scroll.z80 build/scroll.tap \
+    && /usr/local/zesarux/bin/zesarux build/scroll.tap
